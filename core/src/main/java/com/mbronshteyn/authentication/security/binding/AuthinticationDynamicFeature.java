9941fcdaae5575734c49b81361516558b4533ad2
misha <misha@localhost.localdomain>
2018-02-25 23:35:41 -0500

    added

---
+++ core/src/main/java/com/mbronshteyn/authentication/security/binding/AuthinticationDynamicFeature.java
@@ -0,0 +1,36 @@
+package com.mbronshteyn.authentication.security.binding;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.container.DynamicFeature;
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.FeatureContext;
+import javax.ws.rs.ext.Provider;
+
+import org.glassfish.jersey.server.model.AnnotatedMethod;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import com.mbronshteyn.authentication.security.filters.AuthenticationFilter;
+
+@Configuration
+@Provider
+public class AuthinticationDynamicFeature implements DynamicFeature {
+
+	@Value("${auth.service.secret}")
+	private String SECRET;	
+	@Value("${auth.service.issuer}")	
+	private String ISSUER;	
+	
+	@Override
+	public void configure(ResourceInfo resourceInfo, FeatureContext context) {
+
+		final AnnotatedMethod am = new AnnotatedMethod(resourceInfo.getResourceMethod());
+		
+        RolesAllowed ra = am.getAnnotation(RolesAllowed.class);
+        if (ra != null) {
+        	context.register(new AuthenticationFilter(SECRET,ISSUER));
+        }	
+		
+	}
+
+}
---
+++ core/src/main/java/com/mbronshteyn/gameserver/exception/GameServerException.java
@@ -0,0 +1,35 @@
+package com.mbronshteyn.gameserver.exception;
+
+public class GameServerException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String message;
+	private int errorStatus;
+	
+	public GameServerException(String message, int errorStatus) {
+		super(message);
+		this.message = message;
+		this.errorStatus = errorStatus;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public int getErrorStatus() {
+		return errorStatus;
+	}
+
+	public void setErrorStatus(int errorStatus) {
+		this.errorStatus = errorStatus;
+	}
+	
+
+}
